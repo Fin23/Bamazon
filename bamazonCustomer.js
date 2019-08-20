393fa85ffecece6a18eeb1aaa356bee30287ee06
Ross Jameson <Rossjameson1623@gmail.com>
2019-08-20 18:58:37 -0500

    working on tables

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "127.0.0.1",
@@ -12,18 +13,92 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    runSearch();
-    afterConnection();
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connected as id" + connection.threadId);
+    display();
+    shopping();
 });
 
-function afterConnection() {
-    connection.query("SELECT * FROM top5000   ", function (err, res) {
+var display = function(){
+    connection.query("SELECT * FROM products   ", function (err, res) {
         if (err) throw err;
-        console.log(res);
+
+
+        console.log("-------------------------------");
+        console.log("       Welcome to Bamazon      ");
+        console.log("-------------------------------");
+        console.log("");
+        console.log("Find Your Products Below");
+        console.log("");
+        
         // below will disconnect you from DB
         //connection.end();
+  
+
+        var table = new Table({
+            head: ['Product ID', 'Product Description/Name', 'D-id', 'Cost'],
+             colWidths: [12, 50, 8],
+             colAligns: ['center', 'left', 'right'],
+             style: {
+                 head: ['aqua'],
+                 compact: true
+             }
+        });
+
+
+        for (var i = 0; i< res.length; i++){
+            table.push([res[i].id, res[i].product_name, res[i].department_id, res[i].price]);
+        }
+        console.log(table.toString());
+        console.log("");
+    });
+};
+
+function shopping(){
+     inquirer.prompt({
+        name:"action",
+        type:"input",
+        message:"Please enter the Product Id of the item you wish to purchase.",
+        choices: [
+            "Item ID",
+            "Item Name"
+        ]
+    })
+
+    .then(function(answer){
+        switch(answer.action){
+            case "Item ID":
+                itemIdSearch();
+            break;
+            case "Item Name":
+                itemNameSearch();
+            break;
+        }
     });
 }
+
+function itemIdSearch(){
+    console.log(product_name);
+}
+
+//  function itemIdSearch(){
+//     inquirer.prompt({
+//         name: 'id',
+//         type:'input',
+//         message: 'What are you looking for?'
+//    })
+
+// .then(function(answer){
+//         console.log(answer.id);
+
+//         var query = "SELECT * FROM products WHERE Id=?";
+//         connection.query(query,{id: answer.id},
+//             function (err, res){
+//                 if(err) throw err;
+//                 for(var i = 0; i < res.length; i++){
+//                     console.log("all is ok");
+//                 }
+
+//                 });
+// });
