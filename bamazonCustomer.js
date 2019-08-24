b7bf8c7d56575f837a98f3835f35964f813a9610
Ross Jameson <Rossjameson1623@gmail.com>
2019-08-24 18:50:00 -0500

    adding documentation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,12 @@
+
+//required dependencies need installing before use
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table2");
+//end of dependencies
 
+//connecting to my database from mysql
 var connection = mysql.createConnection({
     host: "127.0.0.1",
     // Your port; if not 3306
@@ -15,29 +20,24 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-// connection.connect(function(err){
-//     if(err) throw err;
-//     console.log("connected as id" + connection.threadId);
-//     display();
-//     //
-// });
+//beginning display when you rund node <file name>
 
 var display = function(){
     connection.query("SELECT * FROM products   ", function (err, res) {
         if (err) throw err;
 
 
-        console.log("-------------------------------");
-        console.log("       Welcome to Bamazon      ");
-        console.log("-------------------------------");
+        console.log("-------------------------------------------------");
+        console.log("       Welcome to Bamazon the online Bazaar      ");
+        console.log("-------------------------------------------------");
         console.log("");
-        console.log("Find Your Products Below");
+        console.log("Find Your Wears  Below for reasonable prices");
         console.log("");
         
         // below will disconnect you from DB
         //connection.end();
   
-
+//creates table in terminal that reflects mysql table, reason for cli table2 dependency 
         var table = new Table({
             head: ['id', 'product_name', 'department_id', 'price','stock_quantity'],
              colWidths: [12, 20, 20],
@@ -57,6 +57,7 @@ var display = function(){
         shopping();
     });
 };
+//prompt for product purchase input by user
 
 function shopping(){
      inquirer.prompt({
@@ -68,34 +69,35 @@ function shopping(){
             "Item Name"
         ]
     })
-
+//takes answer and finds it with in the data base and returns information based on what is found 
     .then(function(answer1){
         var selection = answer1.productToBuy;
         connection.query("SELECT * FROM products WHERE Id=?", selection, function(err, res){
             if(err) throw err;
             if(res.length === 0){
-                console.log("That Product doesn't exist, Please enter a Product Id from the list above");
+                console.log("That Product doesn't exist ..idiot, Please enter a Product Id from the list above");
                 shopping();
             }else{
                 inquirer
                 .prompt({
                     name:"quantity",
                     type:"input",
-                    message: "How many items would you like to purchase?"
+                    message: "How many items would you like to purchase from this totally real store?"
                 })
                     .then(function(answer2){
                         var quantity = answer2.quantity;
                         if (quantity > res[0].stock_quantity - quantity){
                             console.log(
-                                "Our Apologies we only have " +
+                                "Our Apologies  you are to greedy " +
                                 res[0].stock_quantity + 
                                 " Items of the product selected"
                             );
                             shopping();
                         }else{
+                            var total = quantity * res[0].price;
                             console.log("");
                             console.log(res[0].product_name + " purchased ");
-                            console.log(quantity + " qty @ $ " + res[0].price);
+                            console.log(quantity + " unit(s) purchased at: $ "+ res[0].price + " total cost $ "+total + " doll hairs");
 
                             var newQuantity = res[0].stock_quantity - quantity;
                             connection.query(
@@ -105,7 +107,7 @@ function shopping(){
                                     if (err) throw err;
                                     console.log("");
                                     console.log("Your Order has been Processed");
-                                    console.log("Thank you for Shopping with us...");
+                                    console.log("Thank you for participating in the capitalist system of supply and demand...");
                                     console.log("");
                                     connection.end();
                                 }
